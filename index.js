9ee6e828bf99058d209884014549886ed5b9adc7
unknown <yamarail.pc@live.jp>
2020-02-16 22:34:52 +0900

    implement simple get api

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const process = require('process');
+
+const PORT = 8080;
+const IP = 'localhost';
+
+let lifeGoal;
+try {
+  lifeGoal = require(`${__dirname}/json/lifeGoal.json`);
+} catch(ex) {
+  console.error('[core]failed to load lifeGoal.json.');
+  process.exit(1);
+}
+
+app.get('/', (req, res) => {
+  res.json(lifeGoal);
+});
+
+app.listen(PORT, IP, () => {
+  console.log(`[core]start listerning at ${PORT}`);
+})
