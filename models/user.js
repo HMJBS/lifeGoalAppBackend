6582a8b3308dcfd082fe93d4c72682638cd89c28
unknown <yamarail.pc@live.jp>
2020-03-08 17:27:16 +0900

    add valiadtor for User.lifeObjects

--- models/user.js
+++ models/user.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const defaultLifeObjects = require(`${__dirname}/json/lifeObjects.json`);
+const lifeObjectsValidator = require(`${__dirname}/models/validators/user.validator.js`);
+
 const UserSchema = new Schema({
 
   userName: {type: String, required: true, max: 20},
-  password: {type: String, required: true, max: 50},
-  lifeObjects: {type: String, default: JSON.stringify(defaultLifeObjects), max: 8192}
+  lifeObjects: {
+    type: String,
+    default: JSON.stringify(defaultLifeObjects),
+    max: 8192,
+    validate: {
+      validator: lifeObjectsValidator,
+      message: 'Invalid lifeObjects'
+    }
+  }
 });
 
 //export model
