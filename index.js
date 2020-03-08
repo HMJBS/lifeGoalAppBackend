11720dfb8015a71f1241778cf7cb009e6406e22b
unknown <yamarail.pc@live.jp>
2020-03-08 20:22:16 +0900

    implemented

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const mongoose = require('mongoose');
 //import mongodb's model
 const User = require(`${__dirname}/models/user.js`);
 
+init();
+
 const PORT = process.env.PORT || 7005;
 const IP = process.env.HOST ||' localhost';
-
-init();
 
 // needed for use req.body of application/json
 app.use(express.json());
@@ -17,30 +17,32 @@ app.use(express.json());
 // server GET User instance
 app.get('/user/:userName', (req, res) => {
   User.findOne( { userName: req.params.userName }, (err, user) => {
+
     if (err) {
+
+      console.error('[GET /user/:userName] error during query');
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    if (user === null) {
       console.log(`[GET /:userName]No entry for ${req.params.userName}`);
       res.status(404).send('Not Found.');
       return;
     }
 
-    let parsedObj;
-    try {
-      parsedObj = JSON.parse(user.lifeObjects);
-    } catch (err) {
+    // remove lifeObjectsStr, instead insert lifeObjects
+    console.log(user);
 
-      console.error(`[GET /:userName] Invalid JSON in User.LifeObjects`);
-      res.status(500).send('Internal Server Error');
-      return;
-
-    }
-    res.status(200).send(parsedObj);
+    res.status(200).send(user);
   });
 });
 
 // serve registering of User instance
-app.post('/user/:userName', (req, res) => {
+app.post('/user', (req, res) => {
 
-  const newUser = new User(req.params.userName);
+  const newUser = new User( { userName: req.body.userName });
   newUser.save((err) => {
     if (err) {
       console.error(`[POST /:userName]Can't save new user ${req.params.userName} on DB.`);
@@ -52,11 +54,11 @@ app.post('/user/:userName', (req, res) => {
   });
 });
 
-app.put('/user/:userName', (req, res) => {
+app.put('/user/:userName', function(req, res) {
 
   User.updateOne( { userName: req.body.userName },
                   { lifeObjects: req.body.lifeObjects },
-                  (err, res) => {
+                  (err, updated) => {
                     if (err) {
                       console.error(`[POST /user/:userName] failed to update`);
                       console.error(err);
