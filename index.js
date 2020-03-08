5bded3d10d429d9efff46859ee0325af9aeaa21f
unknown <yamarail.pc@live.jp>
2020-03-08 17:48:37 +0900

    add PUT request handler

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const IP = process.env.HOST ||' localhost';
 
 init();
 
+// needed for use req.body of application/json
+app.use(express.json());
+
 // server GET User instance
-app.get('/:userName', (req, res) => {
+app.get('/user/:userName', (req, res) => {
   User.findOne( { userName: req.params.userName }, (err, user) => {
     if (err) {
       console.log(`[GET /:userName]No entry for ${req.params.userName}`);
@@ -35,7 +38,7 @@ app.get('/:userName', (req, res) => {
 });
 
 // serve registering of User instance
-app.post('/:userName', (req, res) => {
+app.post('/user/:userName', (req, res) => {
 
   const newUser = new User(req.params.userName);
   newUser.save((err) => {
@@ -49,6 +52,21 @@ app.post('/:userName', (req, res) => {
   });
 });
 
+app.put('/user/:userName', (req, res) => {
+
+  User.updateOne( { userName: req.body.userName },
+                  { lifeObjects: req.body.lifeObjects },
+                  (err, res) => {
+                    if (err) {
+                      console.error(`[POST /user/:userName] failed to update`);
+                      console.error(err);
+                      res.status(500).send('Internal Server Error');
+                      return;
+                    }
+                    res.status(200).send('OK');
+                  });
+});
+
 app.listen(PORT, IP, () => {
   console.log(`[core]start listerning at ${PORT}`);
 });
@@ -57,6 +75,6 @@ function init() {
 
   // load env file
   const dotenv = require('dotenv').config();
-  mongoose.connect(process.env.MONGOOSE_HOST, {userNewUrlParser: true});
+  mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING, {userNewUrlParser: true});
 
 }
