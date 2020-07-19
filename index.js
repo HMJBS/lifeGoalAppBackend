7ea7fc84f7f637aca3bf5de83226594c1774caab
Yohei <yamarail.pc@live.jp>
2020-07-19 11:56:26 +0000

    update model User to contain password hash

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
-const express = require('express');
-const app = express();
-const process = require('process');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require('express')
+const app = express()
+const process = require('process')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const passport = require('passport')
 
 // http method handlers
-const getObjectTree = require('./lib/getObjectTree.js').getObjectTree;
-const postNewUser = require('./lib/postNewUser.js').postNewUser;
-const addNewNonRootObject = require('./lib/addNewNonRootObject.js').addNewNonRootObject;
-const addNewRootObject = require('./lib/addNewRootObject.js').addNewRootObject;
-const removeObject = require('./lib/removeObject.js').removeObject;
+const getObjectTree = require('./lib/getObjectTree.js').getObjectTree
+const postNewUser = require('./lib/postNewUser.js').postNewUser
+const addNewNonRootObject = require('./lib/addNewNonRootObject.js').addNewNonRootObject
+const addNewRootObject = require('./lib/addNewRootObject.js').addNewRootObject
+const removeObject = require('./lib/removeObject.js').removeObject
+
+// passportjs storategy configurator
+const localStorategy = require('./lib/authLogin.js').localStorategy
 
 // load env file
-if(!dotenv.config()) {
-  throw new Error('dotenv failed.');
+if (!dotenv.config()) {
+  throw new Error('dotenv failed.')
 }
 
-init();
+init()
 
-const PORT = process.env.PORT || 7005;
-const IP = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 7005
+const IP = process.env.HOST || 'localhost'
 
 // needed for use req.body of application/json
-app.use(cors());
-app.use(express.json({}));
+app.use(cors())
+app.use(express.json({}))
 
 // get list of all life object tree (instance of SingleObject)
-app.get('/user/:userName', getObjectTree);
+app.get('/user/:userName', getObjectTree)
 
 // register new user
-app.post('/user', postNewUser);
+app.post('/user', postNewUser)
+
+// login
+app.post('/login', passport.authenticate('local'))
 
 // add single oject to non-root oject
-app.put('/user/:userName/:objectId', addNewNonRootObject);
+app.put('/user/:userName/:objectId', addNewNonRootObject)
 
 // add single object to root object
-app.put('/user/:userName/', addNewRootObject);
+app.put('/user/:userName/', addNewRootObject)
 
 // remove object
-app.delete('/user/:userName/:objectId', removeObject);
+app.delete('/user/:userName/:objectId', removeObject)
 
 app.listen(PORT, IP, () => {
-  console.log(`[core]start listerning at ${PORT}`);
-});
+  console.log(`[core]start listerning at ${PORT}`)
+})
 
-function init() {
-
+function init () {
   mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING,
-    {useNewUrlParser: true, useUnifiedTopology: true });
+    { useNewUrlParser: true, useUnifiedTopology: true })
 
+  passport.use(localStorategy)
 }
