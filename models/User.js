9d905a1fe7acf1165d235a7e85a5b55116d0f31e
Yohei <yamarail.pc@live.jp>
2020-07-19 13:49:33 +0000

    add hash to model User

--- models/User.js
+++ models/User.js
@@ -1,16 +1,35 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const SingleObject = require(`${__dirname}/SingleObject.js`);
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
 
   // user name
-  userName: {type: String, required: true, max: 20},
+  userName: { type: String, required: true, max: 20 },
 
-  // Id list of all lifeObject which belong to this user 
-  lifeObjects: [{ type: Schema.Types.ObjectId, ref: 'SingleObject'}]
-});
+  // Id list of all lifeObject which belong to this user
+  lifeObjects: [{ type: Schema.Types.ObjectId, ref: 'SingleObject' }],
 
-//export model
-module.exports = mongoose.model('User', UserSchema);
+  // bcrypt password hash
+  hash: { type: String, max: 60 },
+
+  // salt rounds to create hash
+  saltRounds: { type: Number, required: true }
+})
+
+/**
+ * check rawpassword to match user hash
+ * @param {String} rawPassword password to check hash
+ * @returns {Boolean}
+ */
+UserSchema.methods.validPassword = function (rawPassword) {
+  bcrypt.compare(rawPassword, this.hash)
+    .then((result) => { return result })
+    .catch((err) => {
+      console.error(`error while hash comparing\n${err}`)
+      throw new Error(`error while hash comparing\n${err}`)
+    })
+}
+
+// export model
+module.exports = mongoose.model('User', UserSchema)
